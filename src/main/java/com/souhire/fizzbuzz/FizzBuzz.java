adb8eae2d1f5eb043f3ec93fa94ce6de5c2907c6
Souhire KENAWI <souhirekenawi@gmail.com>
2020-11-27 11:52:46 +0100

    refactor

--- src/main/java/com/souhire/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/souhire/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,6 @@ package com.souhire.fizzbuzz;
 public class FizzBuzz {
 
     public String process(int number) {
-        if (number == 1)
-            return "1";
-        if (number == 2)
-            return "2";
-        return "4";
+        return String.valueOf(number);
     }
 }
