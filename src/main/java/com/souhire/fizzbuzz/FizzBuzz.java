429c87b7989f30a9d7d59890e2f6af031a2a6400
Souhire KENAWI <souhirekenawi@gmail.com>
2020-11-27 11:49:27 +0100

    obvious implem

--- src/main/java/com/souhire/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/souhire/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,8 @@ package com.souhire.fizzbuzz;
 public class FizzBuzz {
 
     public String process(int number) {
-        return "1";
+        if (number == 1)
+            return "1";
+        return "2";
     }
 }
