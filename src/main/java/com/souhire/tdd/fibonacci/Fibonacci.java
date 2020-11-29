2039bcffa57102e05647d677af592f4910ac5cfa
Souhire KENAWI <souhirekenawi@gmail.com>
2020-11-29 17:15:06 +0100

    Fibonacci tdd

--- src/main/java/com/souhire/tdd/fibonacci/Fibonacci.java
+++ src/main/java/com/souhire/tdd/fibonacci/Fibonacci.java
@@ -3,7 +3,10 @@ package com.souhire.tdd.fibonacci;
 public class Fibonacci {
 
     public int calculate(int position) {
-        return 0;
+        if (position == 0 || position == 1)
+            return position;
+        return calculate(position - 1)
+                + calculate(position - 2);
     }
 
 }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -5,8 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciTest {
 
-    @Test void shouldbe0IfPositionIs0() {
+    @Test void shouldBe0IfPositionIs0() {
         assertEquals(0, new Fibonacci().calculate(0));
+    }
+
+    @Test void shouldBe1IfPositionIs1() {
+        assertEquals(1, new Fibonacci().calculate(1));
+    }
+
+    @Test void shouldBe1IfPositionIs2() {
+        assertEquals(1, new Fibonacci().calculate(2));
+    }
+
+    @Test void shouldBe2IfPositionIs3() {
+        assertEquals(2, new Fibonacci().calculate(3));
+    }
+
+    @Test void shouldBe3IfPositionIs4() {
+        assertEquals(3, new Fibonacci().calculate(4));
     }
 
 }
